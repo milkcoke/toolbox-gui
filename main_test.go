f927f92c6a077c7c061d2e376b08eff0b9a0726f
milkcoke <mbh023@gmail.com>
2023-02-19 16:28:03 +0900

    test: Add integration test code

---
+++ main_test.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/test"
+	markdown "github.com/milkcoke/fyne-app/src/markdown"
+	"testing"
+)
+
+// This is integration test
+
+func Test_makeUI(t *testing.T) {
+	var testConfig = markdown.Config
+
+	edit, preview := testConfig.MakeUI()
+
+	var txtMsg = "Falcon never can't die!"
+	test.Type(edit, txtMsg)
+
+	if preview.String() != txtMsg {
+		t.Error("Failed -- didn't find expected value in preview")
+	}
+}
+
+func Test_RunApp(t *testing.T) {
+	var testConfig = markdown.Config
+
+	testApp := test.NewApp()
+	testWindow := testApp.NewWindow("Test markdown")
+
+	edit, preview := testConfig.MakeUI()
+	testConfig.CreateMenuItems(testWindow)
+
+	testWindow.SetContent(container.NewHSplit(edit, preview))
+	testApp.Run()
+
+	var txtMsg = "Some test"
+	test.Type(edit, txtMsg)
+	if preview.String() != txtMsg {
+		t.Error("Failed to run app with correct text message.")
+	}
+}
