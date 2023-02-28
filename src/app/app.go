dbf068dda2ef6f825e10025e1f354142b2d7258a
milkcoke <mbh023@gmail.com>
2023-02-28 19:51:56 +0900

    refactor: Rename installer struct

--- src/app/app.go
+++ src/app/app.go
@@ -1,33 +1,33 @@
 package app
 
-type installer struct {
+type InstallerConfig struct {
 	Name    string
 	Ext     string
 	Url     string
 	version string
 }
 
-var GoInstaller = installer{
+var GoInstaller = InstallerConfig{
 	"Go",
 	".msi",
 	"https://go.dev/dl/go1.20.1.windows-amd64.msi",
 	"1.20.1",
 }
-var DockerInstaller = installer{
+var DockerInstaller = InstallerConfig{
 	"Docker",
 	".exe",
 	"https://desktop.docker.com/win/main/amd64/Docker%20Desktop%20Installer.exe",
 	"23.0",
 }
 
-var NotionInstaller = installer{
+var NotionInstaller = InstallerConfig{
 	"Notion",
 	".exe",
 	"https://www.notion.so/desktop/windows/download",
 	"2.21",
 }
 
-var NodeInstaller = installer{
+var NodeInstaller = InstallerConfig{
 	"Nodejs",
 	".msi",
 	"https://nodejs.org/dist/v18.14.2/node-v18.14.2-x64.msi",
