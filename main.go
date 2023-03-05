ee96ff9326c500f1a4881ed150ef941f1106b3b5
milkcoke <mbh023@gmail.com>
2023-03-06 02:01:33 +0900

    chore: Remove unnecessary comments

--- main.go
+++ main.go
@@ -7,20 +7,16 @@ import (
 )
 
 func main() {
-	// create a fyne app
 	app := app.New()
 
-	// create a window for the app
 	win := app.NewWindow("Falcon Toolbox")
 
 	var config = layout.Config
 	var container = config.LoadImageButtons(win)
 
-	// show window and run app
 	win.Resize(fyne.Size{Width: 800, Height: 480})
 
 	win.SetContent(container)
 
-	//win.CenterOnScreen()
 	win.ShowAndRun()
 }
