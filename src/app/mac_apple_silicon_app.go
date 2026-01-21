cd3e6ec8e412b89ecd8af46185768d28a63e616d
milkcoke <mbh023@gmail.com>
2026-01-22 00:07:45 +0900

    feat: Update the application version in mac apple silicon.

--- src/app/mac_apple_silicon_app.go
+++ src/app/mac_apple_silicon_app.go
@@ -12,21 +12,21 @@ type InstallerConfig struct {
 var GoInstaller = InstallerConfig{
 	"Go",
 	".pkg",
-	"https://go.dev/dl/go1.22.0.darwin-arm64.pkg",
-	"1.22.0",
+	"https://go.dev/dl/go1.25.6.darwin-arm64.pkg",
+	"1.25.6",
 }
 var DockerInstaller = InstallerConfig{
 	"Docker",
 	".dmg",
 	"https://desktop.docker.com/mac/main/arm64/Docker.dmg?utm_source=docker&utm_medium=webreferral&utm_campaign=docs-driven-download-mac-arm64",
-	"25.0.2",
+	"29.1.5",
 }
 
 var NotionInstaller = InstallerConfig{
 	"Notion",
 	".dmg",
 	"https://www.notion.so/desktop/mac-universal/download",
-	"3.1.1",
+	"7.1.0",
 }
 
 var NodeInstaller = InstallerConfig{
@@ -34,32 +34,32 @@ var NodeInstaller = InstallerConfig{
 	".pkg",
 	// pkg supports both intel, apple-silicon
 	// It contains a universal binary that includes both architectures.
-	"https://nodejs.org/dist/v20.11.1/node-v20.11.1.pkg",
-	"v20.11.1",
+	"https://nodejs.org/dist/v24.13.0/node-v24.13.0.pkg",
+	"v24.13.0",
 }
 
 var PostmanInstaller = InstallerConfig{
 	"Postman",
 	".zip",
 	"https://dl.pstmn.io/download/latest/osx_arm64",
-	"10.23",
+	"11.81.1",
 }
 
 var PythonInstaller = InstallerConfig{
 	"Python",
 	".pkg",
-	"https://www.python.org/ftp/python/3.12.2/python-3.12.2-macos11.pkg",
-	"3.12.2",
+	"https://www.python.org/ftp/python/3.14.2/python-3.14.2-macos11.pkg",
+	"3.14.2",
 }
 var VSCodeInstaller = InstallerConfig{
 	"VisualStudioCode",
 	".zip",
 	"https://code.visualstudio.com/sha/download?build=stable&os=darwin-arm64",
-	"1.86.2",
+	"1.108",
 }
 var SlackInstaller = InstallerConfig{
 	"Slack",
 	".dmg",
-	"https://downloads.slack-edge.com/releases/macos/4.36.140/prod/universal/Slack-4.36.140-macOS.dmg",
-	"4.36.140",
+	"https://downloads.slack-edge.com/desktop-releases/mac/universal/4.47.72/Slack-4.47.72-macOS.dmg",
+	"4.47.72",
 }
