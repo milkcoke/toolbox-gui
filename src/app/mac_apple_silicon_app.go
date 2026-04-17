4b63a725ad1ebd96c4b1c34af7ec4563d5ec879b
문승훈 <seunghunmoon023@nexon.co.kr>
2026-04-17 13:33:37 +0900

    + build: Update the installer version

--- src/app/mac_apple_silicon_app.go
+++ src/app/mac_apple_silicon_app.go
@@ -13,13 +13,13 @@ var GoInstaller = InstallerConfig{
 	"Go",
 	".pkg",
 	"https://go.dev/dl/go1.25.6.darwin-arm64.pkg",
-	"1.25.6",
+	"1.26.2",
 }
 var DockerInstaller = InstallerConfig{
 	"Docker",
 	".dmg",
 	"https://desktop.docker.com/mac/main/arm64/Docker.dmg?utm_source=docker&utm_medium=webreferral&utm_campaign=docs-driven-download-mac-arm64",
-	"29.1.5",
+	"29.4.0",
 }
 
 var NotionInstaller = InstallerConfig{
@@ -34,32 +34,32 @@ var NodeInstaller = InstallerConfig{
 	".pkg",
 	// pkg supports both intel, apple-silicon
 	// It contains a universal binary that includes both architectures.
-	"https://nodejs.org/dist/v24.13.0/node-v24.13.0.pkg",
-	"v24.13.0",
+	"https://nodejs.org/dist/v24.15.0/node-v24.15.0.pkg",
+	"v24.15.0",
 }
 
 var PostmanInstaller = InstallerConfig{
 	"Postman",
 	".zip",
 	"https://dl.pstmn.io/download/latest/osx_arm64",
-	"11.81.1",
+	"12.6.3",
 }
 
 var PythonInstaller = InstallerConfig{
 	"Python",
 	".pkg",
-	"https://www.python.org/ftp/python/3.14.2/python-3.14.2-macos11.pkg",
-	"3.14.2",
+	"https://www.python.org/ftp/python/3.14.4/python-3.14.4-macos11.pkg",
+	"3.14.4",
 }
 var VSCodeInstaller = InstallerConfig{
 	"VisualStudioCode",
 	".zip",
 	"https://code.visualstudio.com/sha/download?build=stable&os=darwin-arm64",
-	"1.108",
+	"1.116",
 }
 var SlackInstaller = InstallerConfig{
 	"Slack",
 	".dmg",
-	"https://downloads.slack-edge.com/desktop-releases/mac/universal/4.47.72/Slack-4.47.72-macOS.dmg",
-	"4.47.72",
+	"https://downloads.slack-edge.com/desktop-releases/mac/universal/4.49.81/Slack-4.49.81-macOS.dmg",
+	"4.49.81",
 }
